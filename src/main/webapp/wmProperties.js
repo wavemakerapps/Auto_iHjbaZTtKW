e202a0428ab1141f1803c8d069d9165ac2e154d0
root <root@61949b3d2f95>
2017-07-20 00:31:00 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_iHjbaZTtKW",
+  "homePage" : "Main",
+  "name" : "Auto_iHjbaZTtKW",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
